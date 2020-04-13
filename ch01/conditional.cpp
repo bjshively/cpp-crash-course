2ccee329313bd9f7e5babf3a7a18f601f82d422c
Brad Shively <bshively@gmail.com>
2020-04-13 00:49:02 -0400

    linting ch01

--- ch01/conditional.cpp
+++ ch01/conditional.cpp
@@ -1,10 +1,13 @@
 #include <cstdio>
 
 int main() {
-    int x = 0;
-    if (x > 0) printf("Positive.");
-    else if (x < 0) printf("Negative.");
-    else printf ("Zero.");
-    printf("\n");
-    return 0;
+  int x = 0;
+  if (x > 0)
+    printf("Positive.");
+  else if (x < 0)
+    printf("Negative.");
+  else
+    printf("Zero.");
+  printf("\n");
+  return 0;
 }
